63bdcb58063ee2041c3e6c89dfad3e99a35bf900
hudi <hudi1star@gmail.com>
2012-07-25 08:03:57 +0200

    Issue #5 Start of processor tier

--- tournament-system/tournament-system-processor/src/main/java/org/toursys/processor/service/TournamentService.java
+++ tournament-system/tournament-system-processor/src/main/java/org/toursys/processor/service/TournamentService.java
@@ -1,19 +1,27 @@
 package org.toursys.processor.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Required;
 import org.toursys.repository.model.Player;
+import org.toursys.repository.model.Season;
 import org.toursys.repository.service.TournamentAggregationDao;
 
 public class TournamentService {
 
-	private TournamentAggregationDao tournamentAggregationDao;
+    private TournamentAggregationDao tournamentAggregationDao;
 
-	public void createPlayer(Player player) {
-		tournamentAggregationDao.createPlayer(player);
-	}
+    public void createPlayer(Player player) {
+        tournamentAggregationDao.createPlayer(player);
+    }
 
-	public void setTournamentAggregationDao(
-			TournamentAggregationDao tournamentAggregationDao) {
-		this.tournamentAggregationDao = tournamentAggregationDao;
-	}
+    public List<Season> getAllSeason() {
+        return tournamentAggregationDao.getAllSeason();
+    }
+
+    @Required
+    public void setTournamentAggregationDao(TournamentAggregationDao tournamentAggregationDao) {
+        this.tournamentAggregationDao = tournamentAggregationDao;
+    }
 
 }
