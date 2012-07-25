e8e6d803d71799c1e31c9f976a20840e3a328192
hudi <hudi1star@gmail.com>
2012-07-25 08:06:05 +0200

    Issue #4 Season is cloneable

--- tournament-system/tournament-system-repository/src/main/java/org/toursys/repository/model/Season.java
+++ tournament-system/tournament-system-repository/src/main/java/org/toursys/repository/model/Season.java
@@ -5,31 +5,44 @@ import java.io.Serializable;
 import org.sqlproc.engine.annotation.Pojo;
 
 @Pojo
-public class Season implements Serializable {
+public class Season implements Serializable, Cloneable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private long seasonId;
+    private long seasonId;
 
-	private String name;
+    private String name;
 
-	public Season() {
-	}
+    public Season() {
+    }
 
-	public long getSeasonId() {
-		return seasonId;
-	}
+    public long getSeasonId() {
+        return seasonId;
+    }
 
-	public void setSeasonId(long seasonId) {
-		this.seasonId = seasonId;
-	}
+    public void setSeasonId(long seasonId) {
+        this.seasonId = seasonId;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public Season clone() {
+        Season season = new Season();
+        season.setSeasonId(getSeasonId());
+        season.setName(getName());
+        return season;
+    }
 
 }
