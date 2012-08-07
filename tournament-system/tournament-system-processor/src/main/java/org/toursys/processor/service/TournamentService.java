515e92328176bb94ddaeafd025ca535624b24e2e
Tomas Hudec <tomas.hudec@i.cz>
2012-08-07 14:25:54 +0200

    Issue #5 New methods with season and tournament

--- tournament-system/tournament-system-processor/src/main/java/org/toursys/processor/service/TournamentService.java
+++ tournament-system/tournament-system-processor/src/main/java/org/toursys/processor/service/TournamentService.java
@@ -3,30 +3,59 @@ package org.toursys.processor.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Required;
+import org.toursys.repository.form.TournamentForm;
 import org.toursys.repository.model.Player;
 import org.toursys.repository.model.Season;
+import org.toursys.repository.model.Tournament;
 import org.toursys.repository.service.TournamentAggregationDao;
 
 public class TournamentService {
 
-	private TournamentAggregationDao tournamentAggregationDao;
+    private TournamentAggregationDao tournamentAggregationDao;
 
-	public void createPlayer(Player player) {
-		tournamentAggregationDao.createPlayer(player);
-	}
+    public void createPlayer(Player player) {
+        tournamentAggregationDao.createPlayer(player);
+    }
 
-	public List<Season> getAllSeason() {
-		return tournamentAggregationDao.getAllSeason();
-	}
+    public List<Season> getAllSeason() {
+        return tournamentAggregationDao.getAllSeason();
+    }
 
-	public void createSeason(Season season) {
-		tournamentAggregationDao.createSeason(season);
-	}
+    public void updateSeason(Season season) {
+        tournamentAggregationDao.updateSeason(season);
+    }
 
-	@Required
-	public void setTournamentAggregationDao(
-			TournamentAggregationDao tournamentAggregationDao) {
-		this.tournamentAggregationDao = tournamentAggregationDao;
-	}
+    public void createSeason(Season season) {
+        tournamentAggregationDao.createSeason(season);
+    }
+
+    public void deleteSeason(Season season) {
+        List<Tournament> tournaments = findTournament(new TournamentForm(season));
+        for (Tournament tournament : tournaments) {
+            deleteTournament(tournament);
+        }
+        tournamentAggregationDao.deleteSeason(season);
+    }
+
+    public List<Tournament> findTournament(TournamentForm tournamentForm) {
+        return tournamentAggregationDao.findTournament(tournamentForm);
+    }
+
+    public void updateTournament(Tournament tournament) {
+        tournamentAggregationDao.updateTournament(tournament);
+    }
+
+    public void createTournament(Tournament tournament) {
+        tournamentAggregationDao.createTournament(tournament);
+    }
+
+    public void deleteTournament(Tournament tournament) {
+        tournamentAggregationDao.deleteTournament(tournament);
+    }
+
+    @Required
+    public void setTournamentAggregationDao(TournamentAggregationDao tournamentAggregationDao) {
+        this.tournamentAggregationDao = tournamentAggregationDao;
+    }
 
 }
